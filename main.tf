37c3a624cd7eae560b75957a807dbe6e7447f356
Ragul-6 <ragulnath06@gmail.com>
2024-11-30 16:11:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  ## resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   subscription_id = "0137d880-e1dc-420a-be96-81f219425576"
   features {}
 }
@@ -17,5 +16,5 @@ provider "azurerm" {
 # Create a resource group
 resource "azurerm_resource_group" "terra-demo" {
   name     = "terra-demo-rg"
-  location = "East Us"
+  location = "Central India"
 }
